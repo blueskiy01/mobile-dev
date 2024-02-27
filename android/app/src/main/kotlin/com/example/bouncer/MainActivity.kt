2cc3d45ff6469de851b70c035d2fee6f5c70dfbc
blueskiy01 <korin.lim@gritlab.ax>
2024-02-27 12:19:34 +0200

    restructure

--- android/app/src/main/kotlin/com/example/bouncer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bouncer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/widgets/block.dart
+++
@@ -1,9 +0,0 @@
-class Block {
-  double x, y;
-  bool isHit = false; // To track if the block has been hit
-  static const double width = 50, height = 20; // Block dimensions
-
-  Block(this.x, this.y);
-
-// Add functionality as needed, like handling collision with the ball
-}
